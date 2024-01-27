1523e031bdb4784a9ddaf35d0c788eb4670e5555
AkkiPaul2000 <skypaul2000@gmail.com>
2024-01-27 18:04:39 +0530

    1st commit
    to my pokex with its background and blank components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import Background from './components/Background';
 import './App.css';
+import Wrapper from './sections/Wrapper';
+import './scss/index.scss'
+import Navbar from './sections/Navbar';
+import Footer from './sections/Footer';
 
 function App() {
   return (
-    <div className="App">
-      My Pokedex
+    <div className='main-container' >
+      <Background/>
+      <div className='app'>
+        <Navbar/>
+        <Wrapper/>
+        <Footer/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Background.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import pokeball1 from '../assets/pokeball.png'
+import pokeball2 from '../assets/pokeball2.png'
+function Background() {
+  return (
+    <div className='background'>
+        <img src={pokeball1} className='pokemon pokemon1' />
+        <img src={pokeball2} className='pokemon pokemon2' />
+        <img src={pokeball1} className='pokemon pokemon3' />
+        <img src={pokeball2} className='pokemon pokemon4' />
+        <img src={pokeball1} className='pokemon pokemon5' />
+        <img src={pokeball2} className='pokemon pokemon6' />
+    </div>
+  )
+}
+
+export default Background
