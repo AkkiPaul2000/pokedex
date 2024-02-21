a171c8f55c721c70515a983f8035d6653198c8fa
AkkiPaul2000 <skypaul2000@gmail.com>
2024-02-21 10:56:05 +0530

    small changes to interface added to it

---
+++ src/app/reducers/getPokemonData.ts
@@ -0,0 +1,6 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { genericPokemonType } from "../../utils/Types";
+
+export const getPokemonData=createAsyncThunk("pokemon/randomPokemon",async(pokemons:genericPokemonType)=>{
+    
+})
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,13 +1,22 @@
 import React, { useEffect } from 'react'
 import { getInitialPokemonData } from '../app/reducers/getInitialPokemonData'
 import { useAppDispatch } from '../app/hooks'
+import { useSelector } from 'react-redux'
 
 function Search() {
   const dispatch=useAppDispatch()
-  
+  const {allPokemon}=useSelector(({pokemon})=>pokemon)
   useEffect(()=>{
     dispatch(getInitialPokemonData())
+    
   },[dispatch])
+  useEffect(()=>{
+    if(allPokemon){
+      const clonedPokemon=[...allPokemon]
+      const randomPokemonsID=clonedPokemon.sort(()=>Math.random()-Math.random()).slice(0,20)
+      console.log(randomPokemonsID)
+    }
+  },[allPokemon])
   return (
     <div>Search</div>
   )
